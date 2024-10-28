84e19cd3d78c43d1a9612078823ad3cd5687925a
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-10-28 06:58:49 -0600

    Eliminar descripcion de ejercicos

--- Jedi_Initiate/Bianlori/5_sumatoria_de_los_midiclorianos.js
+++ Jedi_Initiate/Bianlori/5_sumatoria_de_los_midiclorianos.js
@@ -1,30 +1,11 @@
-/* 5. Sumatoria de los midiclorianos (Sumatoria de un rango)
-Contexto temático:
-Qui-Gon Jinn está midiendo el nivel de midiclorianos en la sangre de Anakin.
-Para saber si puede ser el Elegido, Qui-Gon debe calcular la sumatoria de los 
-midiclorianos desde 1 hasta un número dado.
-
-Objetivo:
-Calcula la sumatoria de todos los números entre 1 y un número dado.
-
-Instrucciones:
-
-La función debe recibir un número entero n y devolver la suma de los números del 1 al n.
-Usa un bucle para sumar todos los números.
-Pista:
-La suma de un rango se puede obtener agregando cada número secuencialmente.
- */
+/* 5. Sumatoria de los midiclorianos (Sumatoria de un rango)*/
 
 
 function sumando(numUsuario) {
     let suma = 0
         for (let i = 1; i <= numUsuario; i++ ){
         suma = suma + i; /* toma el valor actual de suma y agrégale el valor de i */
-        
         } 
-        return suma
+    return suma
 }
-
-
-
 console.log("la suma de los primeros 100 numeros es : " + sumando(6) );
