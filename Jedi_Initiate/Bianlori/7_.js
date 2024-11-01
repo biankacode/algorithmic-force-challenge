60515caa675d60932cf7e2546b9c747cd6147cd1
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-10-31 19:48:35 -0600

    El canto de los Ewoks

---
+++ Jedi_Initiate/Bianlori/7_.js
@@ -0,0 +1,12 @@
+/* 7. El canto de los Ewoks (Contador de vocales)*/
+
+function contadorDeVocales(vocales) {
+    for (let i = 0; i < vocales.length; i++){
+        if ("aeiou".includes(vocales[i]) ){
+            return true 
+        }
+    }
+    return false
+
+}
+console.log(contadorDeVocales("gato"))
