04a9fb0653ab950fbe14cce1c2bb30c46a90e250
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-11-15 06:08:39 -0600

    2_ el lado oscuro de las cadenas

--- Jedi_Initiate/Bianlori/10_celsius_a _fahrenheit.js
+++ Jedi_Initiate/Bianlori/10_celsius_a _fahrenheit.js
@@ -1,18 +1,5 @@
-/* 10. La temperatura de Tatooine (Conversión de grados Celsius a Fahrenheit)
-Contexto temático:
-En Tatooine, las temperaturas son extremas. Anakin necesita convertir las temperaturas de
- grados Celsius a Fahrenheit para saber si es seguro salir al desierto.
+/* 10. La temperatura de Tatooine (Conversión de grados Celsius a Fahrenheit)*/
 
-Objetivo:
-Crea una función que convierta una temperatura de grados Celsius a Fahrenheit.
-
-Instrucciones:
-
-La función debe recibir una temperatura en grados Celsius y devolver la temperatura en grados 
-Fahrenheit.
-La fórmula de conversión es: F = (C * 9/5) + 32.
-Pista:
-Aplica la fórmula correctamente dentro de la función para hacer la conversión. */
 function convercion (celsius){
     let resultado = (celsius * 9/5) + 32
     return resultado
--- Jedi_Initiate/Bianlori/9_tabla_de_multiplicar.js
+++ Jedi_Initiate/Bianlori/9_tabla_de_multiplicar.js
@@ -1,17 +1,4 @@
-/* 9. El entrenamiento Jedi (Generar tabla de multiplicar)
-Contexto temático:
-Los aprendices Jedi en el Templo deben memorizar tablas de multiplicar para mejorar su concentración 
-y habilidades matemáticas. Ayuda a Anakin a generar la tabla de multiplicar de un número.
-
-Objetivo:
-Genera la tabla de multiplicar de un número dado.
-
-Instrucciones:
-
-La función debe recibir un número y generar su tabla de multiplicar hasta el 10.
-Muestra los resultados en un formato claro.
-Pista:
-Utiliza un bucle para multiplicar el número dado por los números del 1 al 10. */
+/* 9. El entrenamiento Jedi (Generar tabla de multiplicar) */
 
 function multiplicar (num){
     for(let i = 0; i <= 10; i++){
