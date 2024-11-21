f71c5fb39f17292fd3291c90a14d886aeef42a95
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-11-21 06:15:57 -0600

    3 mayor elemento de una lista

--- Jedi_Padawan/Bianlori/3_mayor_elemento_de_una _lista.js
+++ Jedi_Padawan/Bianlori/3_mayor_elemento_de_una _lista.js
@@ -1,32 +1,16 @@
-/* 3. El Mayor Caballero Jedi (Mayor elemento de una lista)
-Contexto temático:
-En una competición para determinar quién es el Jedi más fuerte, 
-los Padawans deben encontrar al Jedi con el mayor nivel de poder. 
-Debes hacerlo sin usar la Fuerza... ni la función max().
-
-Objetivo:
-Encuentra el elemento más grande de una lista de números.
-
-Instrucciones:
-
-Escribe una función que reciba una lista de números y devuelva el mayor 
-número sin usar la función incorporada max().
-Recorre la lista para comparar cada número.
-Pista:
-Mantén una variable que guarde el número más grande hasta el momento mientras recorres la lista. */
-
-const numeros = [9,10,20]
+/* 3. El Mayor Caballero Jedi */
+const numeros = [9,30,20]
 
 function numeroMayor(numeros) {
-    let mayor = numeros[0];/* iniciamos con la primera pocicion del array */
-    for (let i = 1 ; i < numeros.length; i++){
-      
-        if (numeros [i] > mayor) {// Comparamos el elemento actual con el mayor
-            mayor = numeros[i] // Si es mayor, actualizamos el valor de 'mayor'
+    let numMayor = numeros[0];/* iniciamos con la primera pocicion del array */
+    for (let i = 1 ; i < numeros.length; i++){ 
+        if (numeros [i] > numMayor ) {// Comparamos el elemento actual con el mayor
+            numMayor = numeros[i] // Si es mayor, actualizamos el valor de 'mayor'
         }
     }
-
-    return mayor 
+    return numMayor
 }
-console.log(numeroMayor(numeros));
+let resultado = numeroMayor(numeros)
+console.log(resultado);
+
  
